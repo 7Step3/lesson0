21dda976d54a8ef433e092b52e01f2850dce5297
7Step3 <stepandes15061996@gmail.com>
2024-11-11 16:53:05 +0300

    Delete module_1_6.py

--- module_1_6.py
+++
@@ -1,4 +0,0 @@
-task_count = 12
-hour_count = 1.5
-course_name = "Python"
-print("Курс:",course_name,", всего задач: ",task_count,", затрачено часов: ",hour_count,", среднее время выполнения", hour_count/task_count,"часа")
